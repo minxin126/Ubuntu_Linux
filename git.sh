70cd4f0feb06a23a9125caaff2b1eecc3e47aac2
minxin126 <wangminxin6@126.com>
2018-04-20 19:36:13 +0800

    shadowsocks

---
+++ git.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+git config --global user.email "wangminxin6@126.com"
+
+git config --global user.name "minxin126"
+
+git clone https://github.com/minxin126/ubuntu-linux.git
+#git add .
+#git commit -a -m ""
+#git push origin master
+
