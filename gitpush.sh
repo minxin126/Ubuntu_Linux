08e2fa9d9da21562d66703f22add24eaa98b7062
minxin126 <wangminxin6@126.com>
2018-08-04 15:27:55 +0800

    forgive me

--- gitpush.sh
+++ gitpush.sh
@@ -5,7 +5,7 @@ git config --global user.name "minxin126"
 
 #git clone https://github.com/minxin126/ubuntu-linux.git
 sudo git add -A . 
-sudo git commit -a -m ""
+sudo git commit -a -m "forgive me"
 sudo git pull origin master
 sudo git push origin master
 
