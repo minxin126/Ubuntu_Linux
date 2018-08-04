7c793a7c358512809043e638bd0c7c3ebb86b265
minxin126 <wangminxin6@126.com>
2018-08-04 15:24:10 +0800

    angry

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.email "wangminxin6@126.com"
 git config --global user.name "minxin126"
 
 #git clone https://github.com/minxin126/ubuntu-linux.git
-sudo git add .
+sudo git add -A . 
 sudo git commit -a -m ""
 sudo git pull origin master
 sudo git push origin master
